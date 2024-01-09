af0fdbbea8587f315a91f0f090d7f2c7da010c22
Nora404 <102860857+Nora404@users.noreply.github.com>
2024-01-09 12:04:44 +0100

    start observer

--- src/Find_Drives.java
+++ src/Find_Drives.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Find_Drives {
-    // - Laufwerke[]
+    private List<String> drives = new ArrayList<>();
 
     /**
      * Füllt das Array mit gefundenen Laufwerken
