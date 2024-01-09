de80b1e66105ada5f170f090faec3e01c240ac6c
Nora404 <102860857+Nora404@users.noreply.github.com>
2024-01-09 10:43:07 +0100

    add get uuid class

---
+++ src/Get_UUID.java
@@ -0,0 +1,13 @@
+import java.util.UUID;
+
+public class Get_UUID {
+    
+    /**
+     * Sucht ob es eine UUID gibt
+     * @return
+     */
+    public UUID getUuid(){
+        return // UUID
+    }
+
+}
