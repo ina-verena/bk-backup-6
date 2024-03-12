49a5051fefedcd2d1d33423cf50db93ebb6c5506
Dreamzn <leon.federau@atos.net>
2024-03-12 11:51:17 +0100

    add drive list to gui

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -174,7 +174,12 @@ public class Gui extends JFrame {
      		JPanel drivers = new JPanel();
      		drivers.setBackground(SystemColor.activeCaption);
      		main.add(drivers, "name_drivers");
+
      		JLabel lblNewLabel = new JLabel("Drivers List");
+		for (String drive: find_drive.getDriveListAsString()) {
+			JLabel asdlasd = new JLabel(drive);
+			drivers.add(asdlasd);
+		}
      		lblNewLabel.setForeground(SystemColor.text);
      		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
      		drivers.add(lblNewLabel);	
